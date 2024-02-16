edeff993c8d8bb13d64f4dd298615094eaad53d0
Ayush Dey <nothingnothing0924@gmail.com>
2024-02-16 22:48:38 +0530

    stack with array

---
+++ Sem2/Java/Week 3 _ Array/q2_stack_with_array.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class q2_stack_with_array {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) {
+        System.out.print("Enter Size: ");
+        int n = sc.nextInt();
+        int top=-1;
+        while(true) {
+            
+        }
+
+    }
+}
