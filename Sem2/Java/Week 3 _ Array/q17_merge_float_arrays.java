cd19cd51453053f13976e422e2c517ccbd426ddc
Ayush Dey <nothingnothing0924@gmail.com>
2024-02-20 20:12:57 +0530

    updated week 2 q17

--- Sem2/Java/Week 3 _ Array/q17_merge_float_arrays.java
+++ Sem2/Java/Week 3 _ Array/q17_merge_float_arrays.java
@@ -4,31 +4,32 @@ class q17_merge_float_arrays {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String args[]) {
-        System.out.print("Enter Number of Rows: ");
+        System.out.println("Enter Number of Rows for First Array: ");
         int n = sc.nextInt();
-        System.out.print("Enter Number of Columns: ");
-        int m = sc.nextInt();
-        float ar[][] = new float[n][m];
-        System.out.println("Enter First Matrix Elements: ");
+        float ar[] = new float[n];
+        System.out.println("Enter Array Elements: ");
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                ar[i][j] = sc.nextFloat();
-            }
+            ar[i] = sc.nextFloat();
         }
-        float arr[][] = new float[n][m];
-        System.out.println("Enter Second Matrix Elements: ");
+
+        System.out.println("Enter Number of Rows for Second Array: ");
+        int m = sc.nextInt();
+        float arr[] = new float[n];
+        System.out.println("Enter Array Elements: ");
+        for (int i = 0; i < m; i++) {
+            arr[i] = sc.nextFloat();
+        }
+
+        float a[] = new float[n + m];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = sc.nextFloat();
-                ar[i][j] += arr[i][j];
-            }
+            a[i] = ar[i];
+        }
+        for (int i = 0; i < m; i++) {
+            a[n + i] = arr[i];
         }
         System.out.println("Merged Array: ");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(ar[i][j] + " ");
-            }
-            System.out.println();
+        for (float i : a) {
+            System.out.print(i + " ");
         }
     }
 }
