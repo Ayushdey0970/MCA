50597ecef23a39c0e84946ff69a78973eae09e38
Ayush Dey <nothingnothing0924@gmail.com>
2024-02-22 10:30:04 +0530

    updated week 4

---
+++ Sem2/Java/Week 5 _ Class, Instance, This Keyword/q20_apple_average.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class q20_apple_average {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String args[]) {
+        float ar[]=new float[7];
+        System.out.println("");
+        for()
+    }
+}
