23d4cc460524b285d5de7a735583d39ac4b3feb0
Ayush Dey <nothingnothing0924@gmail.com>
2024-02-22 12:15:39 +0530

    updated week 2 22

--- Sem2/Java/Week 2 _ Variables, Conditions and Loops/q22_armstrong.java
+++ Sem2/Java/Week 2 _ Variables, Conditions and Loops/q22_armstrong.java
@@ -6,10 +6,12 @@ class q22_armstrong {
     public static void main(String args[]) {
         System.out.print("Enter Number: ");
         int n = sc.nextInt();
+        String s = "" + n;
+        int l = s.length();
         int sum = 0, m = n;
         while (m > 0) {
             int r = m % 10;
-            sum = sum + (int) Math.pow(r, 3);
+            sum = sum + (int) Math.pow(r, l);
             m = m / 10;
         }
         if (n == sum) {
