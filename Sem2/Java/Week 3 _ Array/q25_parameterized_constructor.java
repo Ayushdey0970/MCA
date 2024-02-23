2c65a615d342a506f13ae55bedfc83080d4c5864
Arpan Bhattacharya <apubhattacharya71@gmail.com>
2024-02-23 09:17:59 +0530

    ...

--- Sem2/Java/Week 3 _ Array/q25_parameterized_constructor.java
+++ Sem2/Java/Week 3 _ Array/q25_parameterized_constructor.java
@@ -16,11 +16,11 @@ class Student {
 public class q25_parameterized_constructor {
     public static void main(String[] args) {
         
-        Student stu1 = new Student("Ayush", 20);
+        Student stu1 = new Student("Arpan", 21);
         System.out.println("Student 1 Details:");
         stu1.displayDetails();
 
-        Student stu2 = new Student("Dot", 21);
+        Student stu2 = new Student("Frank", 21);
         System.out.println("\nStudent 2 Details:");
         stu2.displayDetails();
     }
