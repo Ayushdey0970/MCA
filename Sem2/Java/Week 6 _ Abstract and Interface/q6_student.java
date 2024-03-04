54caf26d98da25a02d87633b4c92dcf61147614d
Ayush Dey <nothingnothing0924@gmail.com>
2024-03-04 20:41:31 +0530

    added question 5

---
+++ Sem2/Java/Week 6 _ Abstract and Interface/q6_student.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+interface department {
+    String deptName = "";
+    String deptHead = "";
+    void show();
+}
+
+class hostel {
+    String hostelName, hostelLocation;
+    int numberOfRooms;
+}
+
+class q6_student {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) {
+        
+    }
+}
