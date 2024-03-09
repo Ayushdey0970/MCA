aa4d26953671183e0a9b2f217a87c27546ab1279
Ayush Dey <nothingnothing0924@gmail.com>
2024-03-09 15:46:37 +0530

    started q13

---
+++ Sem2/Java/Week 6 _ Abstract and Interface/q13_bank_and_customer.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+interface bank {
+    
+}
+
+class q13_bank_and_customer {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) {
+        
+    }
+}
