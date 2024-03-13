089c045cbd7573e0456746974bb5c8bb2ae0a6c8
Ayush Dey <nothingnothing0924@gmail.com>
2024-03-13 11:42:45 +0530

    q13 incomplete

--- Sem2/Java/Week 6 _ Abstract and Interface/q13_bank_and_customer.java
+++ Sem2/Java/Week 6 _ Abstract and Interface/q13_bank_and_customer.java
@@ -1,12 +1,111 @@
 import java.util.*;
 
 interface bank {
-    
+    float deposit(float amount);
+
+    float withdraw(float amount);
+}
+
+class account implements bank {
+    String name;
+    long accountId;
+    float balance;
+
+    account(String name, long accountId, float balance) {
+        this.name = name;
+        this.accountId = accountId;
+        this.balance = balance;
+    }
+
+    @Override
+    public float deposit(float amount) {
+        return balance + amount;
+    }
+
+    @Override
+    public float withdraw(float amount) {
+        if (balance < amount) {
+            System.out.println("Insufficient Balance");
+            return balance;
+        } else {
+            return balance - amount;
+        }
+    }
+}
+
+class customer extends account {
+
+    customer(String name, long accountId, float balance) {
+        super(name, accountId, balance);
+    }
+
+    void display() {
+        System.out.println("Account Holder Name: " + name);
+        System.out.println("Account Number: " + accountId);
+        System.out.println("Account Balance: " + balance);
+    }
+
+    void create() {
+        System.out.println("Account Created!!");
+    }
+
+    void close() {
+        System.out.println("Account Closed!!");
+    }
 }
 
 class q13_bank_and_customer {
     static Scanner sc = new Scanner(System.in);
+
     public static void main(String args[]) {
-        
+        ArrayList<customer> ar = new ArrayList<>();
+        while (true) {
+            System.out.println(
+                    "1. Create Account\n2. Deposit Amount\n3. Withdraw Amount\n4. Display Details\n5. Close Account\n6. Exit: ");
+            int ch = sc.nextInt();
+            System.out.print("Enter Account Number: ");
+            long accountId = sc.nextLong();
+            boolean flag = false;
+            switch (ch) {
+                case 1:
+                    for (customer c : ar) {
+                        if (c.accountId == accountId) {
+                            System.out.println("Account Already Exists!!");
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (flag) {
+                        break;
+                    } else {
+                        System.out.print("Enter Account Name: ");
+                        String name = sc.next();
+                        System.out.print("Enter Balance: ");
+                        float balance = sc.nextFloat();
+                        customer o = new customer(name, accountId, balance);
+                        ar.add(o);
+                        break;
+                    }
+                case 2:
+                    for(customer c:ar) {
+                        if(c.accountId==accountId) {
+                            System.out.print("Enter Amount to be Deposited: ");
+                            float amount=sc.nextFloat();
+                            c.balance=c.deposit(amount);
+                            flag=true;
+                            break;
+                        }
+                    }
+                    if(flag) {
+                        break;
+                    }
+                    else {
+                        System.out.println("This Account Doesn't Exist");
+                        break;
+                    }
+                    case 3:
+                        
+            }
+        }
     }
 }
