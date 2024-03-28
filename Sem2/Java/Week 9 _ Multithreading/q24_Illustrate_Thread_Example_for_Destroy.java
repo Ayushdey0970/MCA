9adecbe5085954bdea9953d230b13ce2e70d1894
Ayush Dey <nothingnothing0924@gmail.com>
2024-03-28 12:21:43 +0530

    arpan chagol

--- Sem2/Java/Week 9 _ Multithreading/q24_Illustrate_Thread_Example_for_Destroy.java
+++ Sem2/Java/Week 9 _ Multithreading/q24_Illustrate_Thread_Example_for_Destroy.java
@@ -1,13 +1,36 @@
-public class q24_Illustrate_Thread_Example_for_Destroy {
-    public static void main(String[] args) {
-        Thread thread = new Thread(new MyRunnable());
-        thread.start();
-        thread.destroy();
+class MyThread extends Thread {
+    private volatile boolean running = true;
+
+    @Override
+    public void run() {
+        while (running) {
+            try {
+                System.out.println("Thread is running...");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted.");
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println("Thread stopped.");
     }
 
-    static class MyRunnable implements Runnable {
-        public void run() {
-            System.out.println("Thread is running...");
+    public void stopThread() {
+        running = false;
+    }
+}
+
+public class q24_Illustrate_Thread_Example_for_Destroy {
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+        thread.start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        thread.stopThread();
     }
 }
